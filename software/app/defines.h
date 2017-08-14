b95915d1b69d4921f2c39abeec40c48ada5effee
Maximilian Stiefel <stiefel.maximilian@online.de>
2017-08-14 10:54:49 +0200

    Made rectangle signal from DAC work.

---
+++ software/app/defines.h
@@ -0,0 +1,18 @@
+//----------------------------------------------------------------------------------------------------------------------------
+// Project:    	Uppsense
+// Name:	hardware.h
+// Author:	Maximilian Stiefel
+// Date:	09.08.2017
+//
+// Description: 
+//
+//----------------------------------------------------------------------------------------------------------------------------
+
+#ifndef APP_DEFINES_H_
+#define APP_DEFINES_H_
+
+#include "hardware.h"
+
+#define DEBUG_LEVEL 1
+
+#endif /* APP_HARDWARE_H_ */
