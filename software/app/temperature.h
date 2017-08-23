6ae7b44df70d408f8f249745639f1b05371fddf4
Elmar van Rijnswou <elmarvanrijnswou@hotmail.com>
2017-08-23 11:05:36 +0200

    refactoring continues

---
+++ software/app/temperature.h
@@ -0,0 +1,28 @@
+/*
+ * temperature.h
+ *
+ *  Created on: Aug 1, 2017
+ *      Author: Elmar
+ */
+
+#ifndef APP_TEMPERATURE_H_
+#define APP_TEMPERATURE_H_
+
+#include "../include/rijnfel_core/rijnfel_core.h"
+
+namespace rijnfel {
+template<typename Sample, typename Temperature>
+class cTemperatureSensor: cSensor<Sample> {
+public:
+	cTemperatureSensor() {
+
+	}
+	virtual Temperature ConvertSample(Sample sample) = 0;
+	virtual ~cTemperatureSensor() {
+
+	}
+};
+
+}
+
+#endif /* APP_TEMPERATURE_H_ */
---
+++ software/app/web_interface.h
@@ -0,0 +1,127 @@
+/*
+ * web_interface.h
+ *
+ *  Created on: Aug 6, 2017
+ *      Author: Elmar
+ */
+
+#ifndef APP_WEB_INTERFACE_H_
+#define APP_WEB_INTERFACE_H_
+
+//TODO remove this
+#include "ads101x.h"
+
+#include <SmingCore/SmingCore.h>
+#include <rijnfel_core/rijnfel_core.h>
+
+namespace rijnfel {
+#define RAW_CHANNEL 1
+#define RAW_SAMPLES 1000
+class cWebInterface: public cDataSink {
+public:
+	/**
+	 * Returns the global cWebInterface instance.
+	 * Only one webserver should ever be running
+	 * @return global cWebInterface instance
+	 */
+	static cWebInterface *GetInstance() {
+		if (cWebInterface::s_instance == NULL)
+			cWebInterface::s_instance = new cWebInterface();
+		return cWebInterface::s_instance;
+	}
+
+	/// Singleton instance
+	static cWebInterface *s_instance;
+
+public:
+	/**
+	 * Should not be used because it's a singleton
+	 * @param delete
+	 */
+	cWebInterface(cWebInterface const&) = delete;
+
+	/**
+	 * Should not be used because it's a singleton
+	 * @param delete
+	 * @return delete
+	 */
+	cWebInterface& operator=(cWebInterface const&) = delete;
+
+	/**
+	 * Registers the paths and starts the server
+	 */
+	void StartServer();
+
+	/**
+	 * Stops the server
+	 */
+	void StopServer();
+
+	/**
+	 * This is a debug function that prints out the average values
+	 * @todo remove this
+	 */
+	void PrintValues();
+
+	/**
+	 * Destructor
+	 */
+	virtual ~cWebInterface();
+
+	// This is what the future will look like
+	// cDataReceiver m_sensorVal[2];
+	// cDataReceiver m_battery;
+	// cDataReceiver m_ledLife;
+	// cDataReceiver m_temp;
+	// But for now...
+	cDataReceiver m_adc_0;
+	cDataReceiver m_adc_1;
+	cDataReceiver m_adc_2;
+	cDataReceiver m_adc_3;
+
+	virtual void ReceiveCallback(void * i_data, cDataReceiver * i_receiver);
+public:
+	/**
+	 *
+	 * @param i_request
+	 * @param i_response Response to the client, index.html
+	 */
+	void OnIndex(HttpRequest & i_request, HttpResponse & i_response);
+
+	/**
+	 * Function gets called from the mobile to refresh the average values
+	 * @param i_request Request from the client
+	 * @param i_response Response to the client, contains JSON with averages
+	 */
+	void OnRefresh(HttpRequest & i_request, HttpResponse & i_response);
+
+	/**
+	 *
+	 * @param i_request
+	 * @param i_response
+	 */
+	void OnRawUpdate(HttpRequest& i_request, HttpResponse& i_response);
+	void OnConfiguration_json(HttpRequest &request, HttpResponse &response);
+	void OnConfiguration(HttpRequest &request, HttpResponse &response);
+
+	//TODO migrate this
+	WebSocketConnection * m_connectedUser;
+	bool m_sendWebsocket;
+protected:
+	/**
+	 * Initializes the variables
+	 */
+	cWebInterface();
+private:
+	void ResetRawValues();
+	/// Whether the server has started or not
+	bool m_serverStarted;
+	/// Holds the average for all the channels
+	int32_t m_adc_value_average[4];
+	/// Server instance
+	HttpServer m_server;
+};
+
+} /* namespace rijnfel */
+
+#endif /* APP_WEB_INTERFACE_H_ */
